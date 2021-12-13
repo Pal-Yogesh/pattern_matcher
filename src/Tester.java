548543df6a82fcc4fe2fcc2281557dad5b148cf4
Pal-Yogesh <yogeshpal5049@gmail.com>
2021-12-13 13:34:23 +0530

    Treesets

---
+++ src/Tester.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Tester {
+    public static void main(String[] args) {
+        ArrayList arr = new ArrayList();
+         arr.add("aaa");
+         arr.add("bbb");
+         arr.add("null");
+        arr.add("10");
+        arr.add("aaa");
+        HashSet i = new HashSet();
+        System.out.println(i);
+    }
+}
